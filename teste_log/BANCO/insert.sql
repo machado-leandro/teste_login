2522517e515ab17da96c3b171b049c3f2f6ec5cb
machado-leandro <69988365+machado-leandro@users.noreply.github.com>
2021-11-16 16:44:34 -0300

    Add files via upload

---
+++ teste_log/BANCO/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `usuarios` VALUES (NULL, 'Usuário Teste', 'demo', 
+	SHA1( 'demo'), 'usuario@demo.com.br', 1, 1, NOW( ));
+  INSERT INTO `usuarios` VALUES (NULL, 'Administrador Teste', 'admin', 
+  	SHA1('admin' ), 'admin@demo.com.br', 2, 1, NOW( ));
